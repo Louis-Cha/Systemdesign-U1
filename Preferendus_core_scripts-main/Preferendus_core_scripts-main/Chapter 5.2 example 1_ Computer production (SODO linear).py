cc1ade6468f844a36dfaf3c9dc6475b36b1a4e41
Louis-Cha <lchamley@tudelft.nl>
2025-09-24 14:55:19 +0200

    Add files via upload

---
+++ Preferendus_core_scripts-main/Preferendus_core_scripts-main/Chapter 5.2 example 1_ Computer production (SODO linear).py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Polygon
+from scipy.optimize import LinearConstraint, milp
+
+"""
+In this example we will use the scipy.optimize.milp function to search for the optimal number of computers of each 
+type the company should produce to maximize profit. Since this is a rather simple, linear problem, we can use a simple 
+and fast solver like the one provided with scipy. Also, this algorithm can handle mixed integer problems, like the one
+provided here.
+
+For documentation on scipy.optimize.milp please see the website of scipy or use the help() function.
+"""
+
+# first, define the objective function. Since it is linear, we can just provide the coefficients with which x1 and x2
+# are multiplied. Note the -1: we need to maximize, however, milp is a minimization algorithm!
+eq = -1 * np.array([300, 500])
+
+# next, define the constraints. For this we first provide a matrix A with all the coefficients x1 and x2 are multiplied.
+A = np.array([[1, 0], [0, 1], [1, 2]])
+
+# next we determine the upper bounds as vectors
+b_u = np.array([60, 50, 120])
+
+# finally, we need to define the lower bound. In our case, these are taken as 0
+b_l = np.full_like(b_u, 0)
+
+# we can now define the LinearConstraint
+constraints = LinearConstraint(A, b_l, b_u)
+
+# the integrality array will tell the algorithm it should take the variables as integers.
+integrality = np.ones_like(eq)
+
+"""Run the optimization"""
+
+result = milp(c=eq, constraints=constraints, integrality=integrality)
+
+print(f'The optimal solution is for producing {result.x[0]} basic computers and '
+      f'{result.x[1]} advanced computers.')
+
+print(f'This will result in a profit of €{round(-1 * result.fun, 2)}')
+
+"""
+The solution of this example can also be found graphically. This figure is created below. You are not required to 
+understand the code below, so it is not annotated further.
+"""
+
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.grid()
+
+# Draw constraint lines
+ax.hlines(0, -1, 60)
+ax.vlines(0, -1, 120)
+ax.plot(np.linspace(-1, 80, 100) * 0 + 50, np.linspace(-1, 120, 100), color="blue")
+ax.plot(np.linspace(-1, 60, 100), np.linspace(-1, 60, 100) * 0 + 60, color="blue")
+ax.plot(np.linspace(-1, 61, 100), -2 / 1 * np.linspace(-1, 61, 100) + 120, color="blue")
+
+# Draw the feasible region
+feasible_set = Polygon(np.array([[0, 0],
+                                 [0, 60],
+                                 [30, 60],
+                                 [50, 20],
+                                 [50, 0]]),
+                       color="lightgrey")
+ax.add_patch(feasible_set)
+
+# Draw the objective function
+ax.plot(np.linspace(-1, 61, 100), -3 / 5 * np.linspace(-1, 61, 100) + 78, color="orange")
+ax.plot(np.linspace(-1, 37, 100), -3 / 5 * np.linspace(-1, 37, 100) + 20, color="orange")
+ax.plot(np.linspace(-1, 61, 100), -3 / 5 * np.linspace(-1, 61, 100) + 50, color="orange")
+ax.arrow(-2, 30, 0, 40, width=0.05, head_width=1, head_length=2, color="orange")
+ax.text(52, 104, r"$x_2 \leq 50$", size=12)
+ax.text(52, 64, r"$x_1 \leq 60$", size=12)
+ax.text(10, 104, r"$x_1 + 2x_2 \leq 120$", size=12)
+ax.text(6, 20, r"$z = 300x_1 + 500x_2$", size=12)
+ax.text(15, 48, "solution space", size=12)
+
+# Draw the optimal solution
+ax.plot(result.x[1], result.x[0], "*", color="black")
+ax.text(32, 64, "Optimal Solution", size=12)
+plt.xlabel(r"$x_2$ Deluxe model")
+plt.ylabel(r"$x_1$ Standard model")
+plt.show()
---
+++ Preferendus_core_scripts-main/Preferendus_core_scripts-main/Chapter 5.2 example 4_ Building design (MODO non-linear).py
@@ -0,0 +1,97 @@
+import numpy as np
+from scipy.optimize import minimize
+
+"""
+Note that the objectives are programmed as the square root of the actual calculation. This is to improve convergence
+with minimize. See also: 
+https://stackoverflow.com/questions/47443122/pythons-scipy-optimize-minimize-with-slsqp-fails-with-positive-directional-der
+"""
+
+
+def objective_profit(variables):
+    """
+    Objective to maximize the profit. Note that the returned value is multiplied by -1. This is since we use a
+    minimization algorithm.
+    :param variables: list with design variable values
+    """
+    x1 = variables[0]
+    x2 = variables[1]
+    x3 = variables[2]
+
+    floor_area = x1 * x2 
+    facade_area = (x1 + x2) * 2 * x3
+
+    revenues = floor_area * 55
+    costs = facade_area * 3.5 + floor_area * 1.5
+
+    return -1 * np.sqrt(revenues - costs)
+
+
+def objective_energy_use(variables):
+    """
+    Objective to minimize the energy consumption.
+    :param variables: list with design variable values
+    """
+    x1 = variables[0]
+    x2 = variables[1]
+    x3 = variables[2]
+    energy_costs_per_m3 = 0.32
+    return np.sqrt(x1 * x2 * x3 * energy_costs_per_m3)
+
+"""
+Before the optimisation the different constraints and bounds are specified
+"""
+
+def constraint_1(variables):
+    """
+    Constraint for optimization problem
+    :param variables: list with design variable values
+    """
+    x1 = variables[0]
+    x2 = variables[1]
+
+    return 35000 - x1 * x2  # the building footprint cannot be bigger than 35,000 m2
+
+
+def constraint_2(variables):
+    """
+    Constraint for optimization problem
+    :param variables: list with design variable values
+    """
+    return objective_profit(variables) ** 2 - (3e4)  # minimal profit is 30,000 euros
+
+
+# set bounds for all variables
+b1 = (80, 200)  # x1
+b2 = (75, 200)  # x2
+b3 = (3, 12)  # x3
+bounds = (b1, b2, b3)
+
+cons = [{'type': 'ineq', 'fun': constraint_1}, {'type': 'ineq', 'fun': constraint_2}]
+
+"""
+Now, the two objectives are optimized separately
+"""
+
+# due to the non-linearity, it might be needed to increase the maximum number of iterations for minimize
+# see line 3-4 of this code block
+result1 = minimize(objective_profit, x0=np.array([130, 160, 10]), bounds=bounds,
+                   constraints=cons, options={'maxiter': 300})
+result2 = minimize(objective_energy_use, x0=np.array([70, 130, 20]), bounds=bounds,
+                   constraints=cons)
+
+if result1.success:
+    print(
+        f'Objective 1 is optimal for x1 = {round(result1.x[0], 2)}, x2 = {round(result1.x[1], 2)} and '
+        f'x3 = {round(result1.x[2], 2)}. The profit is then €{round(result1.fun ** 2, 2)}.')
+else:
+    print('Problem with optimization (result1), cannot find a solution')
+    print(result1)
+
+if result2.success:
+    print(
+        f'Objective 2 is optimal for x1 = {round(result2.x[0], 2)}, x2 = {round(result2.x[1], 2)} and x3 = '
+        f'{round(result2.x[2], 2)}. The energy costs are then €{round(result2.fun ** 2, 2)}.')
+else:
+    print('Problem with optimization (result2), cannot find a solution')
+    print(result2)
